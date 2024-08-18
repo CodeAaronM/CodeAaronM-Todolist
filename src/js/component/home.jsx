16ded1420cb793bbeffaf09aad189af99b5c5b65
CodeAaronM <aaron.martinez.b@gmail.com>
2024-08-18 07:51:17 +0000

    se puede leer la lista de el api

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,41 +1,43 @@
-import React ,{useState} from "react";
+import React, { useEffect, useState } from "react";
+import Getdata from "./getdata";
 
 //create your first component
 const Home = () => {
 
-	const [todo, setTodo] = useState("")
+	const [todo, setTodo] = useState("");
 	const [todoList, setTodoList] = useState([]);
 	const [counter, setCounter] = useState(0)
 
-function agregar(e){
-    if (e.key === "Enter") { 
-		setTodoList([...todoList, todo]); 
-		setTodo(""); 
-		setCounter(counter+1)
-}}
+	function agregar(e) {
+		if (e.key === "Enter" & todo != "") {
+			setTodoList([...todoList, { label: todo }]);
+			setTodo("");
+			setCounter(counter + 1)
+		}
+	}
 
-function eliminar(index){
-	const nuevaLista = todoList.filter((item, i) => i !== index);
-	setTodoList(nuevaLista)
-}
-
+	function eliminar(index) {
+		const nuevaLista = todoList.filter((item, i) => i !== index);
+		setTodoList(nuevaLista)
+		setCounter(counter - 1)
+	}
 
 	return (
 		<>
-			<h1 className="text-center fw-light" id="titleToDo" style={{fontSize:"70px"}}>Todos</h1>
-		<div className="mx-auto w-50 border mt-5" style={{boxShadow: "0px 10px 0px -4px white, 0px 20px 0px -8px #f1f1f1, 0px 20px 20px 20px rgb(225 225 225)"}}>
-		<input className="form-control form-control-lg" style={{border:'none', boxShadow:'none'}} value={todo}  onChange={ (e) =>(setTodo(e.target.value))} onKeyDown={agregar} placeholder="ingresa tus tareas aqui"></input>
-        {todoList.map((item, index) => (
-		<ul className="list-group">
-			<li className="list-group-item row border-end-0 border-bottom-0 border-start-0 rounded-0 m-0">
-          <label className="col-11 overflow-y-auto" key={index}>{item}</label>
-		  <button type="button" style={{border:'none', background:'none'}} className="col-1 btn btn-outline-light" onClick={() => eliminar(index)}>X</button>
-      </li>
-	  </ul>
-        ))}
-	  <footer className="p-2">{counter} items left</footer>
-	  </div>
-
+			<h1 className="text-center fw-light" id="titleToDo" style={{ fontSize: "70px" }}>Todos</h1>
+			<div className="mx-auto w-50 border mt-5" style={{ boxShadow: "0px 10px 0px -4px white, 0px 20px 0px -8px #f1f1f1, 0px 20px 20px 20px rgb(225 225 225)" }}>
+				<input className="form-control form-control-lg" style={{ border: 'none', boxShadow: 'none' }} value={todo} onChange={(e) => (setTodo(e.target.value))} onKeyDown={agregar} placeholder="ingresa tus tareas aqui"></input>
+				{todoList.map((item, index) => (
+					<ul key={index} className="list-group">
+						<li className="list-group-item row border-end-0 border-bottom-0 border-start-0 rounded-0 m-0">
+							<label className="col-11 overflow-y-auto" key={index}>{item.label}</label>
+							<button type="button" style={{ border: 'none', background: 'none' }} className="col-1 btn btn-outline-light" onClick={() => eliminar(index)}>X</button>
+						</li>
+					</ul>
+				))}
+				<footer className="p-2">{counter} items left</footer>
+			</div>
+			<Getdata setTodoList={setTodoList} />
 		</>
 	);
 };
